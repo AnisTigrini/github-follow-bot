27e90cbfac0c3478ec830f66a8aed67c055b8cfd
Anis Tigrini <anis.tigrini@cit-direct.com>
2024-10-25 09:23:43 -0400

    Adjusting errors

--- follow_bot.py
+++ follow_bot.py
@@ -1,28 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
-import sys
-
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 # Firefox used
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 # base url
 driver.get("http://github.com/login")
 
-username = driver.find_element_by_id("login_field")
-password = driver.find_element_by_id("password")
+username = driver.find_element(By.ID, "login_field")
+password = driver.find_element(By.ID, "password")
 
 # password and username need to go into these values
-username.send_keys("username for github")
+username.send_keys("enter email")
 time.sleep(1)
-password.send_keys("password for github")
+password.send_keys("enter password")
 time.sleep(1)
 
-login_form = driver.find_element_by_xpath("//input[@value='Sign in']")
+login_form = driver.find_element(By.XPATH, "//input[@value='Sign in']")
 time.sleep(1)
 login_form.click()
-time.sleep(1)
+time.sleep(20)
 
 # These are some of the most popular users on github
 prepend = ["jashkenas", "ruanyf", "substack", "kennethreitz", "jlord", "daimajia", "mdo", "schacon", "mattt",
@@ -37,20 +34,21 @@ prepend = ["jashkenas", "ruanyf", "substack", "kennethreitz", "jlord", "daimajia
 
 for user in prepend:
     for t in range(1, 100):
-        string = "https://github.com/{}/followers?page={}".format(user, t)
+        string = "https://github.com/{}/?page={}&tab=followers".format(user, t)
+        print("Following user {} in page {}".format(user, t))
+        
         driver.get(string)
-        time.sleep(1)
+        time.sleep(5)
 
-        # make sure to pick the correct directory to save the files to
-        # follow_button = driver.find_elements_by_xpath("//button[@type='submit']")
-        follow_button = driver.find_elements_by_xpath("//button[@aria-label='Follow this person']")
+        follow_button = driver.find_elements(By.XPATH, "//input[@type='submit'][@value='Follow']")
 
+        print("Follow butrto", follow_button)
         # Once page is loaded this clicks all buttons for follow
         try:
             for i in follow_button:
                 i.submit()
+                time.sleep(2)
         except:
             pass
-        time.sleep(1)
 
 driver.close()
